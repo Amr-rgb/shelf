f3d1d40643eee93683af546b74b32f9c137be087
Amr Taha <amrshiha2@gmail.com>
2022-09-06 23:23:38 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      caudex: ["Caudex", "serif"],
+      poppins: ["poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        offWhite: "#faf5ec",
+        green: "#28353c",
+        lightGreen: "#d7e5e3",
+      },
+    },
+  },
+  plugins: [],
+};
