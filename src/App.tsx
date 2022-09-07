8b1cfa79a37af3b06a08915278526bbd29a297bb
Amr Taha <amrshiha2@gmail.com>
2022-09-07 07:32:49 +0200

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Latest } from "./components/Latest";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App px-4 py-8">
+    <div className="App px-4 py-8 pb-44">
       <Header />
       <Main />
       <Latest />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  QueueListIcon,
+} from "@heroicons/react/24/outline";
+
+export const Navbar = () => {
+  return (
+    <div className="nav py-4 pb-6 fixed bottom-0 left-0 right-0">
+      <div className="px-4 flex items-center justify-between">
+        <div className="rounded-3xl cursor-pointer">
+          <MagnifyingGlassIcon className="p-2 w-11" />
+        </div>
+
+        <div className="bg-white p-5 rounded-3xl">
+          <HomeIcon className="p-2 w-11" />
+        </div>
+
+        <div className="rounded-3xl cursor-pointer">
+          <QueueListIcon className="p-2 w-11" />
+        </div>
+      </div>
+    </div>
+  );
+};
