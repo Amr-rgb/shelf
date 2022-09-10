768fa32a55d88043569b8a4fdaad5697c7250982
Amr Taha <amrshiha2@gmail.com>
2022-09-10 14:59:25 +0200

    adding details of library books

--- src/components/details/BookDetails.tsx
+++ src/components/details/BookDetails.tsx
@@ -1,8 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import { Header } from "./Header";
 
 export const BookDetails = () => {
   const { bookId } = useParams();
+  const location = useLocation();
+  const [loc] = useState(location.pathname.split("/")[1]);
 
   const details = {
     book_id: 896220,
@@ -19,14 +22,18 @@ export const BookDetails = () => {
     <div>
       <Header />
 
-      <div className="space-y-6 bg-white p-4 py-8 rounded-xl">
-        <p className="uppercase font-caudex font-bold text-xl text-center">
-          book details
-        </p>
-        <span className="block w-full h-[1px] bg-lightGreen"></span>
-        {Object.entries(details).map(([title, value]) => (
-          <BookDetail key={title} title={title} value={value} />
-        ))}
+      <div className="space-y-8">
+        {loc === "library" && <UserDetails />}
+
+        <div className="space-y-6 bg-white p-4 py-8 rounded-xl">
+          <p className="uppercase font-caudex font-bold text-xl text-center">
+            book details
+          </p>
+          <span className="block w-full h-[1px] bg-lightGreen"></span>
+          {Object.entries(details).map(([title, value]) => (
+            <BookDetail key={title} title={title} value={value} />
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -54,6 +61,51 @@ const BookDetail = ({ title, value }: { title: any; value: any }) => {
       ) : (
         <p className="text-sm leading-relaxed">{value}</p>
       )}
+    </div>
+  );
+};
+
+const UserDetails = () => {
+  return (
+    <div className="space-y-2">
+      <UserDetail title="pages" value="234" />
+      <UserDetail title="read" value="20" />
+    </div>
+  );
+};
+
+const UserDetail = ({ title, value }: { title: string; value: string }) => {
+  const [active, setActive] = useState(false);
+  const [val, setVal] = useState(value);
+  let input = useRef<HTMLInputElement>(null);
+
+  const clickHandler = () => {
+    setActive(true);
+    input.current?.focus();
+  };
+
+  useEffect(() => {
+    active && input.current?.focus();
+  }, [active]);
+
+  return (
+    <div className="flex items-center">
+      <p className="w-16">{title}</p>
+      <input
+        ref={input}
+        className="ml-4 mr-auto p-3 w-32"
+        type="text"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+        disabled={!active}
+      />
+
+      <button
+        className="p-2 px-4 bg-lightGreen text-green rounded-lg"
+        onClick={clickHandler}
+      >
+        Edit
+      </button>
     </div>
   );
 };
